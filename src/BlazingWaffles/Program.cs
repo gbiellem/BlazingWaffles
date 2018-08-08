b371ebf277769d77457575af15bc22792e136aeb
Simon Cropp <simon.cropp@gmail.com>
2018-08-08 22:55:27 +1000

    .

--- src/BlazingWaffles/Program.cs
+++ src/BlazingWaffles/Program.cs
@@ -4,7 +4,7 @@ namespace BlazingWaffles
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             CreateHostBuilder().Build().Run();
         }
