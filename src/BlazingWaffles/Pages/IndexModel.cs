c8a2f6f2e383c5726efb94fbcc5b2f2b1a85bd52
Simon Cropp <simon.cropp@gmail.com>
2020-01-31 15:22:55 +1100

    add sha

--- src/BlazingWaffles/Pages/IndexModel.cs
+++ src/BlazingWaffles/Pages/IndexModel.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Reflection;
 using Microsoft.AspNetCore.Components;
 using PropertyChanged;
 using WaffleGenerator;
@@ -8,6 +10,12 @@ namespace BlazingWaffles
     public partial class IndexModel :
         ComponentBase
     {
+        public IndexModel()
+        {
+            var informationalVersion = GetType().Assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>().Single().InformationalVersion;
+            Sha = informationalVersion.Substring(informationalVersion.IndexOf('+') + 1);
+        }
+
         public string Waffle
         {
             get
@@ -21,6 +29,7 @@ namespace BlazingWaffles
             }
         }
 
+        public string Sha { get; }
         public int Paragraphs { get; set; } = 1;
         public bool IncludeHeading { get; set; }
         public OutputType OutputType { get; set; }
