f500d919171475bbd8a54beb14b838766c4c052e
SimonCropp <simon.cropp@gmail.com>
2020-05-22 21:12:56 +1000

    refs

--- src/BlazingWaffles/Program.cs
+++ src/BlazingWaffles/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using BlazingWaffles;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,7 +11,13 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault();
         builder.RootComponents.Add<App>("app");
-        builder.Services.AddBaseAddressHttpClient();
+
+        builder.Services.AddTransient(
+            provider => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
         return builder.Build().RunAsync();
     }
 }
