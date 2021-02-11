31778e32a5b7307c945f9ebed01eaa3f1ddeaa25
Simon Cropp <simon.cropp@gmail.com>
2021-02-11 12:59:13 +1100

    Update Program.cs

--- src/BlazingWaffles/Program.cs
+++ src/BlazingWaffles/Program.cs
@@ -1,18 +1,8 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
-using BlazingWaffles;
+﻿using BlazingWaffles;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using TextCopy;
 
-        var builder = WebAssemblyHostBuilder.CreateDefault();
-        builder.Services.InjectClipboard();
-        builder.RootComponents.Add<App>("app");
-
-        builder.Services.AddTransient(
-            _ => new HttpClient
-            {
-                BaseAddress = new(builder.HostEnvironment.BaseAddress)
-            });
-
-        await builder.Build().RunAsync();
+var builder = WebAssemblyHostBuilder.CreateDefault();
+builder.Services.InjectClipboard();
+builder.RootComponents.Add<App>("app");
+await builder.Build().RunAsync();
