8cace66b3b2a639b72e5f066a80a5d7ae97fa3a8
Simon Cropp <simon.cropp@gmail.com>
2021-02-15 17:08:17 +1100

    refs

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 using ImageMagick;
+using Verify.AngleSharp;
 using VerifyTests;
 
 public static class ModuleInitializer
@@ -10,7 +11,17 @@ public static class ModuleInitializer
         // remove some noise from the html snapshot
         VerifierSettings.ScrubEmptyLines();
         VerifierSettings.ScrubLinesWithReplace(s => s.Replace("<!--!-->", ""));
+        HtmlPrettyPrint.All();
+        VerifierSettings.ScrubLinesWithReplace(s =>
+        {
+            var indexOf = s.IndexOf("sha256-");
+            if (indexOf == -1)
+            {
+                return s;
+            }
 
+            return s.Substring(0, indexOf) + s.Substring(indexOf + 51);
+        });
         VerifyPlaywright.Enable();
         VerifyImageMagick.Initialize();
         VerifyImageMagick.RegisterComparers(.01, ErrorMetric.Fuzz);
